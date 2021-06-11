530a6f2b11650022fa062d1b4821eec1ad7a0ff9
Jimmy Su <jmysu@users.noreply.github.com>
2021-06-11 11:00:55 +0800

    Add three more statemachine for PyBase LEDs

--- firmware/PlatformIO.projects/picoPIOPWM/src/pwm.c
+++ firmware/PlatformIO.projects/picoPIOPWM/src/pwm.c
@@ -38,13 +38,25 @@ int pioPWM() {
     uint offset = pio_add_program(pio, &pwm_program);
     //printf("Loaded program at %d\n", offset);
 
-    pwm_program_init(pio, sm, offset, PICO_DEFAULT_LED_PIN);
-    pio_pwm_set_period(pio, sm, (1u << 16) - 1);
+    pwm_program_init(pio, 0, offset, PICO_DEFAULT_LED_PIN);
+    pio_pwm_set_period(pio, 0, (1u << 16) - 1);     //Pico DEFAULT GREEN LED
 
+    pwm_program_init(pio, 1, offset, 18);           //PyBase RED LED
+    pio_pwm_set_period(pio, 1, (1u << 15) - 1);
+    pwm_program_init(pio, 2, offset, 19);           //PyBase ORANGER LED
+    pio_pwm_set_period(pio, 2, (1u << 14) - 1);
+    pwm_program_init(pio, 3, offset, 20);           //PyBase BLUE LED
+    pio_pwm_set_period(pio, 3, (1u << 13) - 1);
+    
     int level = 0;
     while (true) {
         //printf("Level = %d\n", level);
-        pio_pwm_set_level(pio, sm, level * level);
+        pio_pwm_set_level(pio, 0, level * level);
+        
+        pio_pwm_set_level(pio, 1, level * level);
+        pio_pwm_set_level(pio, 2, level * level);
+        pio_pwm_set_level(pio, 3, level * level);
+        
         level = (level + 1) % 256;
         sleep_ms(10);
     }
